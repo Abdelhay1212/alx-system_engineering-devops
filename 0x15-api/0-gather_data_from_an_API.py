ef27f38f01d5edf03735f09701bebda90f4e9799
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-10-24 15:54:34 +0200

    updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,13 +13,13 @@ def main():
     usersURL = f'https://jsonplaceholder.typicode.com/users/{ID}'
     response = requests.get(usersURL)
     response = response.json()
-    EMP_NAME = response["name"]
+    EMPLOYEE_NAME = response["name"]
 
     todosURL = f'https://jsonplaceholder.typicode.com/todos?userId={ID}'
     response = requests.get(todosURL)
     response = response.json()
 
-    TOTAL_TASKS = len(response)
+    TOTAL_NUMBER_OF_TASKS = len(response)
 
     doneTasks = []
     numOfDoneTasks = 0
@@ -28,13 +28,13 @@ def main():
             numOfDoneTasks += 1
             doneTasks.append(dic["title"])
 
-    NUM_TASKS = numOfDoneTasks
+    NUMBER_OF_DONE_TASKS = numOfDoneTasks
 
-    print(
-        f'Employee {EMP_NAME} is done with tasks({NUM_TASKS}/{TOTAL_TASKS}):')
+    print(f"Employee {EMPLOYEE_NAME} is "
+          f"done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
     for task in doneTasks:
-        print(task)
+        print(f"     {task}")
 
 
 if __name__ == "__main__":
