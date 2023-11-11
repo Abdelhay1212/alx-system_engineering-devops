2afbb32f942dbb2104e8b046c298805fa6507390
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-11-11 05:10:21 +0100

    updated

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,27 +3,26 @@
 import requests
 
 
-def recurse(subreddit, count=None, hot_list=[]):
+def recurse(subreddit, after=None):
     """
     recursive function that queries the Reddit API and returns a list
     containing the titles of all hot articles for a given subreddit.
     """
 
-    if count == 0:
-        return hot_list
-
-    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit={count}'
-    headers = {
-        'user-agent': 'Abdelhay/1.0'
-    }
-
-    response = requests.get(URL, headers=headers)
-    if response.status_code != 200:
-        print(None)
-        return
-
-    data = response.json()['data']['children']
-
-    count = len(data) - 1
-    hot_list.append(data[count]['data']['title'])
-    return recurse(subreddit, count - 1, hot_list=hot_list)
+    headers = {"User-Agent": "Abdelhay/1.0"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers=headers, params=params)
+    all_posts = []
+    if response.status_code == 200:
+        data = response.json()
+        after = data["data"]["after"]
+        if after is None:
+            return all_posts
+        for post in data["data"]["children"]:
+            all_posts.append(post["data"]["title"])
+        next = recurse(subreddit, after)
+        all_posts.extend(next)
+        return all_posts
+    return None
